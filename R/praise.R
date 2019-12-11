8419d11ec07ccca60b7f3a8642f27b8a17427392
kristyrobledo <robledo.kristy@gmail.com>
2019-12-11 15:32:10 +1100

    This commit will update the documentation and add a default for the punctuation.

--- R/praise.R
+++ R/praise.R
@@ -1,7 +1,7 @@
 #' Delivers praise
 #'
 #' @description This function is useful when you are
-#' feeling sad and your name is Kristy.
+#' feeling sad.
 #' @param name Text string of the name I would like to praise
 #' @param punctuation This is our punctionation as a text input
 #' @return Returns text string with praise
@@ -10,7 +10,7 @@
 #' @examples
 #' praise(name = "Kristy", punctuation="!")
 #'
-praise <-function(name, punctuation){
+praise <-function(name, punctuation="!"){
   paste0("You're the best, ",name, punctuation)
 }
 
@@ -19,3 +19,12 @@ praise <-function(name, punctuation){
 
 ## license - mit license
 ## usethis::use_mit_license("Kristy Robledo")
+
+# in the terminal:
+##git config --global user.name 'kristy.robledo'
+##git config --global user.email 'robledo.kristy@gmail.com'
+##git config --global --list
+
+#this is stolen from github repo:
+#git remote add origin https://github.com/kristyrobledo/praiseMe.git
+#git push -u origin master
