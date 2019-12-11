a5fd5929c0e85cf38e792270ae3d5af73d848de7
kristyrobledo <robledo.kristy@gmail.com>
2019-12-11 15:50:37 +1100

    Updating the documentation

--- R/praise.R
+++ R/praise.R
@@ -1,7 +1,7 @@
 #' Delivers praise
 #'
 #' @description This function is useful when you are
-#' feeling sad.
+#' feeling sad, and would like to recieve some praise.
 #' @param name Text string of the name I would like to praise
 #' @param punctuation This is our punctionation as a text input
 #' @return Returns text string with praise
